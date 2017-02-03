5a5210be9bf94b3bcda8b07d1d1c8b242fe11859
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2017-02-03 17:36:31 +0300

    About page: added some dummy data. Sidebar: changed colors.

--- client/app/sysinfo/sysinfo.controller.js
+++ client/app/sysinfo/sysinfo.controller.js
@@ -5,6 +5,17 @@ angular.module('projectsApp')
     //*******************************************
     // Start up code
 
+    $scope.serial = "X12DND05";
+    $scope.firmwareVer = "1.03";
+    $scope.soundEncoding = "PCM";
+    $scope.timestamp = moment();
+    $scope.uptime = "10:12:33";
+    $scope.sysLoad = "10%";
+    $scope.diskUsage = "77%";
+    $scope.macAddress = "78:00:9E:22:E9:CE";
+    $scope.gateway = "10.10.12.161";
+    $scope.dnsServer = "dns1.mgch.ru";
+
     // Start up code
     //*******************************************
 
